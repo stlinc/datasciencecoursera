137d826ea2747ad48fc2cf5188663e6493eaa2aa
stlinc <fred14lin@yahoo.com>
2016-04-08 11:49:19 +0800

    Update Week 2 practice.R

--- Statistical-Inference/Week-2/Week 2 practice.R
+++ Statistical-Inference/Week-2/Week 2 practice.R
@@ -151,3 +151,31 @@ coverage <- sapply(lambdavals, function(lambda){
 })
 # plot CI results vs 95%
 ggplot(data.frame(lambdavals, coverage), aes(x = lambdavals, y = coverage)) + geom_line(size = 2) + geom_hline(yintercept = 0.95) + ylim(0, 1.0)
+
+#Quiz
+# Q 2: Suppose that diastolic blood pressures (DBPs) for men aged 35-44 are normally distributed with a mean of 80 (mm Hg) and a standard deviation of 10. About what is the probability that a random 35-44 year old has a DBP less than 70?
+mu <- 80; sd <- 10
+pnorm(70,mean = mu, sd = sd)
+
+# Q3: Brain volume for adult women is normally distributed with a mean of about 1,100 cc for women with a standard deviation of 75 cc. What brain volume represents the 95th percentile?
+mu <- 1100; sd <- 75
+qnorm(0.95,mean = mu, sd = sd)
+
+# Q4: Follow Q3, Consider the sample mean of 100 random adult women from this population. What is the 95th percentile of the distribution of that sample mean?
+n <- 100
+qnorm(0.95, mean = mu, sd = sd/sqrt(n))
+
+#Q5: You flip a fair coin 5 times, about what's the probability of getting 4 or 5 heads?
+n <- 5; p <- 0.5
+pbinom(3, size = 5, prob = 0.5, lower.tail = FALSE)
+
+# Q6: The respiratory disturbance index (RDI), a measure of sleep disturbance, for a specific population has a mean of 15 (sleep events per hour) and a standard deviation of 10. They are not normally distributed. Give your best estimate of the probability that a sample mean RDI of 100 people is between 14 and 16 events per hour?
+mu <- 15; sd <- 10; n<- 100
+q <- c(14, 16)
+p <- pnorm(q, mean = mu, sd = sd/sqrt(n))
+p14_16 <- p[2]-p[1]
+
+#Q8: The number of people showing up at a bus stop is assumed to be Poisson with a mean of 5 people per hour. You watch the bus stop for 3 hours. About what's the probability of viewing 10 or fewer people?
+lambda <- 5; t <- 3
+prob <- ppois(10, lambda = lambda*t)
+
